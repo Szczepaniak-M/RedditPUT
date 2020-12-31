167df09a748519ba810b8e282b186922dab6cf9a
Szczepaniak-M <m.szczepaniak99@gmail.com>
2020-12-31 12:08:26 +0100

    Client option framework

--- server/client-thread.c
+++ server/client-thread.c
@@ -6,12 +6,41 @@ void *clientThread(void *inputData) {
     ServerStatus *status = data->status;
     int error;
     char textBuffer[300];
+    int requestType = -1;
+    int requestComplete = 0;
+    int size = 0;
     while ((error = read(descriptor, textBuffer, 300)) && error > 0) {
-        pthread_mutex_lock(&status->mutex);
+        // todo buffor, czytanie do czasu znalezienia typu requestu
 
-        pthread_mutex_unlock(&status->mutex);
+        switch (requestType) {
+            case 1: // rejestracja
+                error = signUp(status, size, textBuffer);
+                requestType = -1;
+                break;
+            case 2: // logowanie
+                error = login(status, size, textBuffer);
+                requestType = -1;
+                break;
+            case 3: // dodaj postll
+                error = addPost(status, size, textBuffer);
+                requestType = -1;
+                break;
+            case 4: // dodaj kanał
+                error = addChannel(status, size, textBuffer);
+                requestType = -1;
+                break;
+            case 5: // zasubskrybuj kanał
+                error = subscribeChannel(status, size, textBuffer);
+                requestType = -1;
+                break;
+            case 6: // usuń subksrypcję
+                error = unsubscribeChannel(status, size, textBuffer);
+                requestType = -1;
+                break;
+            default:
+                break;
+        }
     }
-
     if (error == -1) {
 
     }
@@ -19,7 +48,7 @@ void *clientThread(void *inputData) {
     for (int i = 0; i < ACTIVE_USER_LIMIT; i++) {
         if (status->activeUsers[i].descriptor == descriptor) {
             status->activeUsers[i].descriptor = -1;
-            if (status->isCleaning == 0){
+            if (status->isCleaning == 0) {
                 status->pthreads[i].isInitialized = 0;
             }
         }
@@ -28,4 +57,28 @@ void *clientThread(void *inputData) {
     free(inputData);
     close(descriptor);
     pthread_exit(NULL);
+}
+
+int signUp(ServerStatus *status, int size, char *buffer) {
+
+}
+
+int login(ServerStatus *status, int size, char *buffer) {
+
+}
+
+int addPost(ServerStatus *status, int size, char *buffer) {
+
+}
+
+int addChannel(ServerStatus *status, int size, char *buffer) {
+
+}
+
+int subscribeChannel(ServerStatus *status, int size, char *buffer) {
+
+}
+
+int unsubscribeChannel(ServerStatus *status, int size, char *buffer) {
+
 }
--- server/client-thread.h
+++ server/client-thread.h
@@ -9,4 +9,16 @@
 
 void *clientThread(void *data);
 
+int signUp(ServerStatus *status, int size, char *buffer);
+
+int login(ServerStatus *status, int size, char *buffer);
+
+int addPost(ServerStatus *status, int size, char *buffer);
+
+int addChannel(ServerStatus *status, int size, char *buffer);
+
+int subscribeChannel(ServerStatus *status, int size, char *buffer);
+
+int unsubscribeChannel(ServerStatus *status, int size, char *buffer);
+
 #endif //SERVER_CLIENT_THREAD_H
